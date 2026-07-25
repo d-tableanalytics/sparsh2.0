51354246f277bef8afa31dbb78c274495b2ee128
d-tableanalytics <rahulyadav@dtableanalytics.com>
2026-07-25 13:00:59 +0530

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -24,6 +24,9 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      varsIgnorePattern: '^[A-Z_]',
+      argsIgnorePattern: '^_',
+      caughtErrors: 'none',
     },
   },
 ])
